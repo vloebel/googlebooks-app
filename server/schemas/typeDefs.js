f404efeb28781cce25e9e53ba7b173a012b8c17f
Vicky Loebel <vickyloebel@gmail.com>
2021-05-06 19:32:04 -0700

    working on typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,8 +6,8 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
-    savedBooks:[bookSchema]
-  
+    savedBooks:[Book]
+    link:String
   }
 
 
@@ -26,6 +26,57 @@ const typeDefs = gql`
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(sss description:String!, title: String!, bookId: Id!, image: image, link:link): User
     
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     addThought(thoughtText: String!): Thought
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
